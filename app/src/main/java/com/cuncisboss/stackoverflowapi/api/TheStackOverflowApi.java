ace104cb5f3641b206e165e876226bc0f06e6938
Green Deahan <greendeahan@gmail.com>
2019-01-29 18:13:23 +0700

    Second Commit

---
+++ app/src/main/java/com/cuncisboss/stackoverflowapi/api/TheStackOverflowApi.java
@@ -0,0 +1,13 @@
+package com.cuncisboss.stackoverflowapi.api;
+
+import com.cuncisboss.stackoverflowapi.model.UsersStackOverflow;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
+public interface TheStackOverflowApi {
+
+    @GET("/2.2/users?page=1&pagesize=5&order=desc&site=stackoverflow")
+    Call<UsersStackOverflow> getUsers(@Query("sort") String sort);
+}
